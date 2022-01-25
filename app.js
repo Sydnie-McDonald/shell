f2b81a56735be2362b6e3d6e95fbf4c999f35be2
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-25 12:57:43 -0600

    adding button functionality

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 // import functions and grab DOM elements
 const caseButton = document.getElementById('case-button');
 const case2Button = document.getElementById('case2-button');
@@ -11,58 +13,44 @@ const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
 
+const cases = ['case', 'case2', 'case3'];
 // let state
+let totalScore = 0;
+let totalWins = 0;
 
 // set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+
+// get user input
+// use user input to update state 
+// update DOM to reflect the new state
 
 // initialize state
-/*
-const hidingPlaces = ['tree', 'shed', 'boulder'];
-
-let correctGuesses = 0;
-let totalGuesses = 0;
-
-shedButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'shed');
-});
-
-treeButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'tree');
-});
-
-boulderButton.addEventListener('click', () => {
-    const hidingSpot = Math.floor(Math.random() * 3);
-    const answer = hidingPlaces[hidingSpot];
-    handleGuess(answer, 'boulder');
-});
-
-function handleGuess(correctSpot, userGuess) {
-    // reset the styles
-    shedContainer.classList.remove('face');
-    treeContainer.classList.remove('face');
-    boulderContainer.classList.remove('face');
-    // then increment the guesses
-    if (userGuess === correctSpot) {
-        correctGuesses++;
-        totalGuesses++;
-
-        const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
-        correctHidingPlaceEl.classList.add('face');
-
-        console.log(correctHidingPlaceEl);
-        winsEl.textContent = correctGuesses;
-        totalEl.textContent = totalGuesses;
-    } else {
-        totalGuesses++;
-        lossesEl.textContent = totalGuesses - correctGuesses;
-    }
-    // then add the face class to that element so that the face shows up
-    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
-}
+caseButton.addEventListener('click', () => {
+  // reset the reveal class for all the balls
+  case1.classList.remove('money');
+  case2.classList.remove('money');
+  case3.classList.remove('money');
+  // increment total plays (total++)
+  totalScore++;
+  // let the computer decide where the ball is hidden (using Math.random)
+  //    i.e. generate a random number between 1 and 3
+  //    store in variable called ballLocation
+  const moneyLocation = Math.ceil(Math.random() * 3);
+  console.log('money is in location', moneyLocation);
+  // update the reveal class for whichever ball was chosen
+  //   case.classList.add('money')
+  if (moneyLocation === 1) {
+    case1.classList.add('money');
+    totalWins++;
+  } else if (moneyLocation === 2) {
+    case2.classList.add('money');
+  } else {
+    case3.classList.add('money');
+  }
+  // update our state
+  //    if ball is under shell # 1 -- increment wins
+  //    update the wins / losses / totals spans with new state
+  totalEl.textContent = totalScore;
+  winsEl.textContent = winsEl;
+  lossesEl.textContent = totalScore - totalWins;
+}); 
