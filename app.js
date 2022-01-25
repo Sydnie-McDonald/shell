6d647c92eef1112dc71713f08fc353b2eb3e2073
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-25 13:32:21 -0600

    had to fix setting issues. Problems with indents etc.

--- app.js
+++ app.js
@@ -19,38 +19,44 @@ let totalScore = 0;
 let totalWins = 0;
 
 // set event listeners 
+case1Button.addEventListener('click', () => {
+    const caseSelect = Math.floor(Math.random() * 3);
+    const answer = cases[caseSelect];
+    handleGuess(answer, 'case1');
+});
 
-// get user input
-// use user input to update state 
-// update DOM to reflect the new state
+case2Button.addEventListener('click', () => {
+    const caseSelect = Math.floor(Math.random() * 3);
+    const answer = cases[caseSelect];
+    handleGuess(answer, 'case2');
+});
 
-// initialize state
-caseButton.addEventListener('click', () => {
-  // reset the reveal class for all the balls
-  case1.classList.remove('money');
-  case2.classList.remove('money');
-  case3.classList.remove('money');
-  // increment total plays (total++)
-  totalScore++;
-  // let the computer decide where the ball is hidden (using Math.random)
-  //    i.e. generate a random number between 1 and 3
-  //    store in variable called ballLocation
-  const moneyLocation = Math.ceil(Math.random() * 3);
-  console.log('money is in location', moneyLocation);
-  // update the reveal class for whichever ball was chosen
-  //   case.classList.add('money')
-  if (moneyLocation === 1) {
-    case1.classList.add('money');
-    totalWins++;
-  } else if (moneyLocation === 2) {
-    case2.classList.add('money');
-  } else {
-    case3.classList.add('money');
-  }
-  // update our state
-  //    if ball is under shell # 1 -- increment wins
-  //    update the wins / losses / totals spans with new state
-  totalEl.textContent = totalScore;
-  winsEl.textContent = winsEl;
-  lossesEl.textContent = totalScore - totalWins;
-}); 
+case3Button.addEventListener('click', () => {
+    const caseSelect = Math.floor(Math.random() * 3);
+    const answer = cases[caseSelect];
+    handleGuess(answer, 'case3');
+});
+
+function handleGuess(correctSpot, userGuess) {
+    // reset the styles
+    caseContainer.classList.remove('money');
+    case2Container.classList.remove('money');
+    case3Container.classList.remove('money');
+    // then increment the guesses
+    if (userGuess === correctSpot) {
+        correctGuesses++;
+        totalGuesses++;
+
+        const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
+        correctHidingPlaceEl.classList.add('money');
+
+        console.log(correctHidingPlaceEl);
+        winsEl.textContent = correctGuesses;
+        totalEl.textContent = totalGuesses;
+    } else {
+        totalGuesses++;
+        lossesEl.textContent = totalGuesses - correctGuesses;
+    }
+    // then add the face class to that element so that the face shows up
+    // update the DOM to show this change to the user (including the losses, not tracked directly in state)
+}
