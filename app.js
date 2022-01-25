6ac2d710a945bf5553f3fbb739f900732e9ace10
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-25 13:37:50 -0600

    fixed variable names and working

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
 // import functions and grab DOM elements
-const caseButton = document.getElementById('case-button');
+const case1Button = document.getElementById('case1-button');
 const case2Button = document.getElementById('case2-button');
 const case3Button = document.getElementById('case3-button');
 
-const caseContainer = document.getElementById('case-container');
+const case1Container = document.getElementById('case-container');
 const case2Container = document.getElementById('case2-container');
 const case3Container = document.getElementById('case3-container');
 
@@ -39,23 +39,23 @@ case3Button.addEventListener('click', () => {
 
 function handleGuess(correctSpot, userGuess) {
     // reset the styles
-    caseContainer.classList.remove('money');
+    case1Container.classList.remove('money');
     case2Container.classList.remove('money');
     case3Container.classList.remove('money');
     // then increment the guesses
     if (userGuess === correctSpot) {
-        correctGuesses++;
-        totalGuesses++;
+        totalWins++;
+        totalScore++;
 
         const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
         correctHidingPlaceEl.classList.add('money');
 
         console.log(correctHidingPlaceEl);
-        winsEl.textContent = correctGuesses;
-        totalEl.textContent = totalGuesses;
+        winsEl.textContent = totalWins;
+        totalEl.textContent = totalScore;
     } else {
-        totalGuesses++;
-        lossesEl.textContent = totalGuesses - correctGuesses;
+        totalScore++;
+        lossesEl.textContent = totalScore - totalWins;
     }
     // then add the face class to that element so that the face shows up
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
