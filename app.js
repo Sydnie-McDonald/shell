931bcce36425629b431639977c39fbdae0953efc
Sydnie McDonald <sydnie.r.mcdonald@gmail.com>
2022-01-28 11:50:18 -0600

    created displayresults and a rest function

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const case3Button = document.getElementById('case3-button');
 const case1Container = document.getElementById('case-container');
 const case2Container = document.getElementById('case2-container');
 const case3Container = document.getElementById('case3-container');
+const results = document.getElementById('results');
 
 const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
@@ -37,26 +38,29 @@ case3Button.addEventListener('click', () => {
     handleGuess(answer, 'case3');
 });
 
-function handleGuess(correctSpot, userGuess) {
-    // reset the styles
+function reset() {
     case1Container.classList.remove('money');
     case2Container.classList.remove('money');
     case3Container.classList.remove('money');
+}
+
+function displayResults() {
+    winsEl.textContent = totalWins;
+    totalEl.textContent = totalScore;
+    lossesEl.textContent = totalScore - totalWins;
+}
+function handleGuess(correctSpot, userGuess) {
+    // reset the styles
+    reset();
     // then increment the guesses
     if (userGuess === correctSpot) {
         totalWins++;
-        totalScore++;
-
         const correctHidingPlaceEl = document.getElementById(`${correctSpot}-container`);
         correctHidingPlaceEl.classList.add('money');
-
         console.log(correctHidingPlaceEl);
-        winsEl.textContent = totalWins;
-        totalEl.textContent = totalScore;
-    } else {
-        totalScore++;
-        lossesEl.textContent = totalScore - totalWins;
     }
+    totalScore++;
+    displayResults();
     // then add the face class to that element so that the face shows up
     // update the DOM to show this change to the user (including the losses, not tracked directly in state)
 }
